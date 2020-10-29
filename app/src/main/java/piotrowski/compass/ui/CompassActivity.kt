cd96cb1e691fed87124cd18546f33c03d605fc25
Marcin Piotrowski <marcin.piotrowski@di-support.com>
2020-10-29 11:37:10 +0100

    add compass view

---
+++ app/src/main/java/piotrowski/compass/ui/CompassActivity.kt
@@ -0,0 +1,55 @@
+package piotrowski.compass.ui
+
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import piotrowski.compass.databinding.ActivityCompassBinding
+
+private val TAG = CompassActivity::class.java.simpleName
+
+@AndroidEntryPoint
+class CompassActivity : AppCompatActivity() {
+
+    private val viewModel: CompassViewModel by viewModels()
+    private val binding: ActivityCompassBinding by lazy {
+        ActivityCompassBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        viewModel.northAzimuth.observe(this, ::displayNewAzimuth)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.startCompass()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        viewModel.stopCompass()
+    }
+
+    private fun displayNewAzimuth(azimuth: Float) {
+        val animation = RotateAnimation(
+            -viewModel.currentAzimuth, -azimuth,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 290
+            repeatCount = 0
+            fillAfter = true
+        }
+
+        viewModel.currentAzimuth = azimuth
+
+        binding.compassNeedle.startAnimation(animation)
+    }
+}
