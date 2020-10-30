ada8db768fe64a2c2b28fef98941b4df74346f0c
Marcin Piotrowski <marcin.piotrowski@di-support.com>
2020-10-30 09:34:39 +0100

    add hilt annotations

--- app/src/main/java/piotrowski/compass/dependency_injection/SystemServicesModule.kt
+++ app/src/main/java/piotrowski/compass/dependency_injection/SystemServicesModule.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.hardware.SensorManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 
+@Module
+@InstallIn(ApplicationComponent::class)
 object SystemServicesModule {
     @Provides
     fun provideSensorManager(@ApplicationContext context: Context): SensorManager =
